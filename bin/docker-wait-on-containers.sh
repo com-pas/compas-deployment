0108cc38b93a123c2347bbc93c10b794778e5410
Dennis Labordus <dennis.labordus@alliander.com>
2021-10-27 15:54:01 +0200

    First step in Action to execute Integration Tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- bin/docker-wait-on-containers.sh
+++ bin/docker-wait-on-containers.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Check until no more process found with filter.
-while [ `docker ps --filter ${1} -q | wc -l` -gt 0 ]
+docker ps -a --filter label=compas
+
+while [ `docker ps --filter ${1} --filter label=compas -q | wc -l` -gt 0 ]
 do
   echo "Still waiting for the containers with filter '${1}'."
   sleep 10
 done
+
 echo "Done no containers with filter '${1}'."
-docker ps -a
+docker ps -a --filter label=compas
