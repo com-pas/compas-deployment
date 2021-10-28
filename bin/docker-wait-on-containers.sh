efece107ee970b51ad19c098b3c88745768c730e
Dennis Labordus <dennis.labordus@alliander.com>
2021-10-28 08:33:12 +0200

    Using IP Address of Docker Container.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- bin/docker-wait-on-containers.sh
+++ bin/docker-wait-on-containers.sh
@@ -3,11 +3,11 @@
 # Check until no more process found with filter.
 docker ps -a --filter label=compas
 
-while [ `docker ps --filter ${1} --filter label=compas -q | wc -l` -gt 0 ]
+while [ `docker ps --filter health=starting --filter label=compas -q | wc -l` -gt 0 ]
 do
-  echo "Still waiting for the containers with filter '${1}'."
+  echo "Still waiting for the containers with filter 'health=starting'."
   sleep 10
 done
 
-echo "Done no containers with filter '${1}'."
+echo "Done no containers with filter 'health=starting'."
 docker ps -a --filter label=compas
