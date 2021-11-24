037963b046cae9a7bddab771e2628f18cce2fd60
Dennis Labordus <dennis.labordus@alliander.com>
2021-11-24 13:30:44 +0100

    Changed the healthy check for PostgreSQL.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- bin/docker-wait-on-containers.sh
+++ bin/docker-wait-on-containers.sh
@@ -7,7 +7,10 @@ while [ `docker ps --filter health=starting --filter label=compas -q | wc -l` -g
 do
   echo "Still waiting for the containers with filter 'health=starting'."
   sleep 10
+  docker ps -a --filter label=compas
 done
+
+docker logs compas_scl-data-service_1
 
 echo "Done no containers with filter 'health=starting'."
 docker ps -a --filter label=compas
